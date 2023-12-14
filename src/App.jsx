7e7aa6da556a4d753c8fac8e38b98a0b4857b6d6
Onur Esin <onuresn034@gmail.com>
2023-12-14 19:50:34 +0300

    app jsx kısmında ki tüm kodlar çalışıyor, mobil tarafı %100 bitti

--- src/App.jsx
+++ src/App.jsx
@@ -18,21 +18,29 @@ function App() {
   }
 
   const handleRegisterClick = () => {
-    if (isValid) {
-      setShowPopup(true);
+    if (isValid && email.trim() !== '') {
+      if (!showPopup) {
+        setShowPopup(true);
+      }
     }
   };
 
   const handleClosePopup = () => {
     setShowPopup(false);
-  }
+  };
   return (
     <>
       <div className="inner">
         {showPopup ? (
       <div className="popup">
-        <h3>Üye olduğunuz için teşekkürler!</h3>
-        <p>Onay postası {email} adresine gönderildi. Lütfen açın ve içindeki onay butonuna tıklayarak üyeliğinizi başlatın.</p>
+        <span className='popup-top'>
+          <img src="check.svg" alt="big-check" />
+          <span className='popup-top-child'>
+            <h3>Üye olduğunuz için teşekkürler!</h3>
+            <p>Onay postası <span className='bold-mail'>{email}</span> adresine gönderildi. Lütfen açın ve içindeki onay butonuna tıklayarak üyeliğinizi başlatın.</p>
+        </span>
+        </span>
+        
         <button className='close-btn' onClick={handleClosePopup}>Pencereyi Kapat</button>
       </div>
     ) : (
@@ -44,15 +52,17 @@ function App() {
           <span className="top">
             <h4>Güncel Kalın!</h4>
             <h6>Aylık güncellemeler alan 60.000'den fazla ürün yöneticisine katılın:</h6>
-            <ul>
+            <ul className='check-list'>
               <li><img src="check.svg" alt="checked"/>Ürünleri keşfet ve senin için önemli olanı inşa et.</li>
               <li><img src="check.svg" alt="checked"/>Ölçüm güncellemelerin başarılı olmasını sağlamak için.</li>
               <li><img src="check.svg" alt="checked"/>Ve çok daha fazlası!</li>
             </ul>
           </span>
-          <span>E-Posta Adres:</span>
-          <input type="text" placeholder='eposta@şirket.com' value={email} onChange={handleEmailChange} style={{borderColor: isValid ? '' : '#FF6155', backgroundColor: isValid ? '':'#FF6155'}} />
-          {!isValid && <div style={{ color: '#FF6155' }}>Geçersiz mail adresi</div>}    
+          <span className='mail-text'>
+            <h6>E-Posta Adresi</h6> 
+            {!isValid && <div className='invalid-mail' style={{color: '#FF6155'}}>Geçersiz mail adresi</div>}
+            </span>
+          <input className='mail-type-sec' type="text" value={email} onChange={handleEmailChange} style={{borderColor: isValid ? '' : '#FF6155', backgroundColor: isValid ? '':'#FF6155'}} />
           <button className='hav-reg' onClick={handleRegisterClick} disabled={!isValid}>Havadise Aylık Kaydol</button>
         </div>
         <div className="web-sec">
